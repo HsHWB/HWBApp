03c4fc5aafa3e5818310a6ef8fb48e73847048d8
HsHWB <Hswbinforgithub@gmail.com>
2019-04-27 00:06:41 +0800

    #引进retrofit2，创建basecomponent模块来写基类，引进core模块来写基础工具

--- app/src/main/java/com/huehn/initword/MainActivity.java
+++ app/src/main/java/com/huehn/initword/MainActivity.java
@@ -2,12 +2,22 @@ package com.huehn.initword;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.Observable;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
