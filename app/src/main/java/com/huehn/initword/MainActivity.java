ba40fa67841e8eb7bbd50d1e34de824f6d9de195
HsHWB <hewenbin@huya.com>
2019-05-01 17:54:56 +0800

    #RxUit的一些工具封装

--- app/src/main/java/com/huehn/initword/MainActivity.java
+++ app/src/main/java/com/huehn/initword/MainActivity.java
@@ -4,12 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.huehn.initword.basecomponent.base.BaseActivity;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.Observable;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     private TextView textView;
 
--- core/src/main/java/com/huehn/initword/core/rxUtils/RxUtils.java
+++ core/src/main/java/com/huehn/initword/core/rxUtils/RxUtils.java
@@ -1,7 +1,96 @@
 package com.huehn.initword.core.rxUtils;
 
+import io.reactivex.Observable;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+
 public class RxUtils {
 
+    /**
+     * 关闭观察
+     * @param disposable
+     */
+    public static void close(Disposable disposable){
+        if (disposable != null && !disposable.isDisposed()){
+            disposable.dispose();
+        }
+    }
 
+    /**
+     * 是否还有观察
+     * @param disposable
+     * @return
+     */
+    public static boolean isUnDisposed(Disposable disposable){
+        return disposable == null || disposable.isDisposed();
+    }
 
+    /**
+     * 在io线程中运行
+     * @param runnable
+     */
+    public static void runOnIOThread(Runnable runnable){
+        Observable.just(runnable)
+            .observeOn(Schedulers.io())
+            .subscribe(new Consumer<Runnable>() {
+                @Override
+                public void accept(Runnable runnable) throws Exception {
+                    if (runnable != null) {
+                        runnable.run();
+                    }
+                }
+            }, new Consumer<Throwable>() {
+                @Override
+                public void accept(Throwable throwable) throws Exception {
+                    throwable.printStackTrace();
+                }
+            });
+    }
+
+    /**
+     * 在一个新线程中运行  使用Schedulers.io()的好处在于它使用线程池的事实，而Schedulers.newThread()没有。
+     * @param runnable
+     */
+    public static void runOnNewThread(Runnable runnable){
+        Observable.just(runnable)
+            .observeOn(Schedulers.newThread())
+            .subscribe(new Consumer<Runnable>() {
+                @Override
+                public void accept(Runnable runnable) throws Exception {
+                    if (runnable != null) {
+                        runnable.run();
+                    }
+                }
+            }, new Consumer<Throwable>() {
+                @Override
+                public void accept(Throwable throwable) throws Exception {
+                    throwable.printStackTrace();
+                }
+            });
+    }
+
+    /**
+     * 在UI线程中进行
+     * @param runnable
+     */
+    public static void runOnUIThread(Runnable runnable){
+        Observable.just(runnable)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(new Consumer<Runnable>() {
+                @Override
+                public void accept(Runnable runnable) throws Exception {
+                    if (runnable != null) {
+                        runnable.run();
+                    }
+                }
+            }, new Consumer<Throwable>() {
+                @Override
+                public void accept(Throwable throwable) throws Exception {
+                    throwable.printStackTrace();
+                }
+            });
+    }
 }
