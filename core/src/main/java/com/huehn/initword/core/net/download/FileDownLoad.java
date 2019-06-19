1458ac338356dc83f12e16b228a81cfc5d3fe5a1
HWB <1298896420@qq.com>
2019-06-19 14:16:58 +0800

    #HttpConnection

--- core/src/main/java/com/huehn/initword/core/net/download/FileDownLoad.java
+++ core/src/main/java/com/huehn/initword/core/net/download/FileDownLoad.java
@@ -1,9 +1,29 @@
 package com.huehn.initword.core.net.download;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import io.reactivex.functions.Consumer;
 
+/**
+ * 下载文件
+ *
+ *
+ * begin  ->  读取初始url  ->  将url转变成URL类对象  ->  打开URL连接 url对象.openConnection()
+ * ->  连接设置httpUrlConnection.setRequestMethod("GET")
+ *
+ *
+ *
+ *
+ * @param <T>
+ */
 public class FileDownLoad<T> {
 
+    //https://github.com/git-for-windows/git/releases/download/v2.22.0.windows.1/Git-2.22.0-64-bit.exe
     private String url;
     private T t;
     private Consumer<T> onSuccessListener;
@@ -16,6 +36,23 @@ public class FileDownLoad<T> {
             this.onErrorListener = builder.onErrorListener;
         }
     }
+
+
+    public void downLoadFile(){
+        BufferedInputStream bufferedInputStream = null;
+        BufferedOutputStream bufferedOutputStream = null;
+
+    }
+
+
+    private HttpURLConnection getConnection(String url) throws IOException {
+        URL url1 = new URL(url);
+        //返回一个 URLConnection 对象，它表示到 URL 所引用的远程对象的连接。
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url1.openConnection();
+        httpURLConnection.setRequestMethod("GET");
+        httpURLConnection.setRequestProperty("Content-Type", "application/octet-stream");
+    }
+
 
 
 
