2d8632ad73d92cb23de4ac302e90a2afa7920a8f
HWB <1298896420@qq.com>
2019-06-24 10:39:39 +0800

    #莫名其妙的冲突

---
+++ app/src/main/java/com/huehn/initword/component/activity/MainActivity.java
@@ -0,0 +1,49 @@
+package com.huehn.initword.component.activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.huehn.initword.R;
+import com.huehn.initword.basecomponent.base.BaseActivity;
+import com.huehn.initword.core.net.response.security.ShangHaiPlateListResponse;
+import com.huehn.initword.core.net.service.security.SecuritiesApi;
+import com.huehn.initword.core.utils.Log.LogManager;
+
+import io.reactivex.functions.Consumer;
+
+public class MainActivity extends BaseActivity {
+
+    public final static String TAG = "MainActivity";
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.text);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addDisposable(SecuritiesApi.getShanghaiPlateList().subscribe(new Consumer<ShangHaiPlateListResponse>() {
+                    @Override
+                    public void accept(ShangHaiPlateListResponse shangHaiPlateListResponse) throws Exception {
+
+                        if (shangHaiPlateListResponse == null || shangHaiPlateListResponse.getShowapi_res_body() == null ||
+                                shangHaiPlateListResponse.getShowapi_res_body().getList() == null){
+                            return;
+                        }
+
+                        LogManager.w(LogManager.CORE_EXCEPTION_LOG_FILE_LOCAL, TAG, shangHaiPlateListResponse);
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        throwable.printStackTrace();
+                    }
+                }));
+            }
+        });
+    }
+}
