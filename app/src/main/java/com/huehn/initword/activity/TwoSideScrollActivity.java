6942d725ce210e25636958abcf23860b67abe9cc
HWB <1298896420@qq.com>
2019-08-30 21:20:19 +0800

    #搞一个两边滑动的view

---
+++ app/src/main/java/com/huehn/initword/activity/TwoSideScrollActivity.java
@@ -0,0 +1,15 @@
+package com.huehn.initword.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.huehn.initword.basecomponent.base.BaseActivity;
+
+public class TwoSideScrollActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
