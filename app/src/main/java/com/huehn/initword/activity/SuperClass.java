13fac6a0938b8131d41377895d3e4f1bc87f7dd6
HWB <1298896420@qq.com>
2019-12-04 14:23:33 +0800

    #kotlin toUInt改成toInt

---
+++ app/src/main/java/com/huehn/initword/activity/SuperClass.java
@@ -0,0 +1,39 @@
+package com.huehn.initword.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.TextView;
+
+import com.huehn.initword.R;
+import com.huehn.initword.basecomponent.base.BaseActivity;
+import com.huehn.initword.core.utils.Log.LogManager;
+
+public class SuperClass extends BaseActivity {
+
+    private TextView superText;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_super_layout);
+        initView();
+        setListener();
+    }
+
+    private void initView(){
+        superText = findViewById(R.id.text);
+        LogManager.d("huehn override supper initView");
+    }
+
+    private void setListener(){
+        LogManager.d("huehn override supper setListener");
+        superText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LogManager.d("huehn override super");
+            }
+        });
+    }
+
+}
