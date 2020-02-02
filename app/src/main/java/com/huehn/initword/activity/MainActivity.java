09c4ef044f577cc534a715f89dd98e22d7391875
HsHWB <hswbinforgithub@gmail.com>
2020-02-02 14:51:08 +0800

    #提交裁剪框

--- app/src/main/java/com/huehn/initword/activity/MainActivity.java
+++ app/src/main/java/com/huehn/initword/activity/MainActivity.java
@@ -1,7 +1,19 @@
 package com.huehn.initword.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
@@ -11,12 +23,16 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.huehn.initword.BuildConfig;
 import com.huehn.initword.R;
 import com.huehn.initword.basecomponent.base.BaseActivity;
 import com.huehn.initword.basecomponent.bean.permission.PermissionRequestCode;
 import com.huehn.initword.basecomponent.bean.permission.PermissionResult;
 import com.huehn.initword.bean.SubTestData;
 import com.huehn.initword.bean.TestData;
+import com.huehn.initword.component.activity.ClipActivity;
+import com.huehn.initword.component.activity.ClipImageActivity;
+import com.huehn.initword.component.activity.clip.FileUtil;
 import com.huehn.initword.core.login.FbLoginMgr;
 import com.huehn.initword.core.module.IOnCallBack;
 import com.huehn.initword.core.net.download.FileDownLoad;
@@ -33,6 +49,7 @@ import com.huehn.initword.ui.anim.module.AnimationSetAnim;
 import com.huehn.initword.ui.anim.module.TranslateAnim;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
+import java.io.File;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Proxy;
@@ -70,8 +87,20 @@ public class MainActivity extends BaseActivity {
     public TextView kotlinText;
     @BindView(R.id.goto_class_override)
     public TextView overrideClass;
+    @BindView(R.id.imageview2)
+    public ImageView headImg;
     public FbLoginMgr fbLoginMgr;
-
+    private File tempFile;
+    //请求相机
+    private static final int REQUEST_CAPTURE = 100;
+    //请求相册
+    private static final int REQUEST_PICK = 101;
+    //请求截图
+    private static final int REQUEST_CROP_PHOTO = 102;
+    //请求访问外部存储
+    private static final int READ_EXTERNAL_STORAGE_REQUEST_CODE = 103;
+    //请求写入外部存储
+    private static final int WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 104;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -82,22 +111,22 @@ public class MainActivity extends BaseActivity {
         fbLoginMgr.doInit();
 
 
-        addDisposable(requestPermissions(PermissionRequestCode.WRITE_EXTERNAL_STORAGE, new String[]{PermissionRequestCode.WRITE_EXTERNAL_STORAGE_STRING})
-            .subscribe(new Consumer<PermissionResult>() {
-                @Override
-                public void accept(PermissionResult permissionResult) throws Exception {
-                    if (permissionResult != null && permissionResult.isGranted()){
-                        ImageLoader imageLoader = ImageLoader.getInstance();
-                        imageLoader.displayImage("http://pic15.nipic.com/20110628/1369025_192645024000_2.jpg", imageView);
-                        LogManager.d("MainActivity", permissionResult.getPermissionString());
-                    }
-                }
-            }, new Consumer<Throwable>() {
-                @Override
-                public void accept(Throwable throwable) throws Exception {
-                    throwable.printStackTrace();
-                }
-            }));
+//        addDisposable(requestPermissions(PermissionRequestCode.WRITE_EXTERNAL_STORAGE, new String[]{PermissionRequestCode.WRITE_EXTERNAL_STORAGE_STRING})
+//            .subscribe(new Consumer<PermissionResult>() {
+//                @Override
+//                public void accept(PermissionResult permissionResult) throws Exception {
+//                    if (permissionResult != null && permissionResult.isGranted()){
+//                        ImageLoader imageLoader = ImageLoader.getInstance();
+//                        imageLoader.displayImage("http://pic15.nipic.com/20110628/1369025_192645024000_2.jpg", imageView);
+//                        LogManager.d("MainActivity", permissionResult.getPermissionString());
+//                    }
+//                }
+//            }, new Consumer<Throwable>() {
+//                @Override
+//                public void accept(Throwable throwable) throws Exception {
+//                    throwable.printStackTrace();
+//                }
+//            }));
 
     }
 
@@ -158,26 +187,37 @@ public class MainActivity extends BaseActivity {
     }
 
     private void clickText(){
-        addDisposable(SecuritiesApi.getShanghaiPlateList().subscribe(new Consumer<ShangHaiPlateListResponse>() {
-            @Override
-            public void accept(ShangHaiPlateListResponse shangHaiPlateListResponse) throws Exception {
-
-                if (shangHaiPlateListResponse == null || shangHaiPlateListResponse.getShowapi_res_body() == null ||
-                        shangHaiPlateListResponse.getShowapi_res_body().getList() == null){
-                    return;
-                }
-
-                LogManager.w(LogManager.CORE_EXCEPTION_LOG_FILE_LOCAL, TAG, shangHaiPlateListResponse);
-                LogManager.d(TAG, shangHaiPlateListResponse);
-                ImageLoader imageLoader = ImageLoader.getInstance();
-                imageLoader.displayImage("http://pic15.nipic.com/20110628/1369025_192645024000_2.jpg", imageView);
-            }
-        }, new Consumer<Throwable>() {
-            @Override
-            public void accept(Throwable throwable) throws Exception {
-                throwable.printStackTrace();
-            }
-        }));
+        //权限判断
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            //申请WRITE_EXTERNAL_STORAGE权限
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    WRITE_EXTERNAL_STORAGE_REQUEST_CODE);
+        } else {
+            //跳转到调用系统相机
+            gotoCamera();
+        }
+//        gotoCamera();
+//        addDisposable(SecuritiesApi.getShanghaiPlateList().subscribe(new Consumer<ShangHaiPlateListResponse>() {
+//            @Override
+//            public void accept(ShangHaiPlateListResponse shangHaiPlateListResponse) throws Exception {
+//
+//                if (shangHaiPlateListResponse == null || shangHaiPlateListResponse.getShowapi_res_body() == null ||
+//                        shangHaiPlateListResponse.getShowapi_res_body().getList() == null){
+//                    return;
+//                }
+//
+//                LogManager.w(LogManager.CORE_EXCEPTION_LOG_FILE_LOCAL, TAG, shangHaiPlateListResponse);
+//                LogManager.d(TAG, shangHaiPlateListResponse);
+//                ImageLoader imageLoader = ImageLoader.getInstance();
+//                imageLoader.displayImage("http://pic15.nipic.com/20110628/1369025_192645024000_2.jpg", imageView);
+//            }
+//        }, new Consumer<Throwable>() {
+//            @Override
+//            public void accept(Throwable throwable) throws Exception {
+//                throwable.printStackTrace();
+//            }
+//        }));
     }
 
     private void goToWeex(){
@@ -309,14 +349,105 @@ public class MainActivity extends BaseActivity {
     }
 
     private void goToOverrideClass(){
-        Intent intent = new Intent(MainActivity.this, SubClass.class);
+        Intent intent = new Intent(MainActivity.this, ClipActivity.class);
         MainActivity.this.startActivity(intent);
+//        gotoCamera();
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        fbLoginMgr.onActivityResult(requestCode, resultCode, data);
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        fbLoginMgr.onActivityResult(requestCode, resultCode, intent);
+
+        switch (requestCode) {
+            case REQUEST_CAPTURE: //调用系统相机返回
+                if (resultCode == RESULT_OK) {
+                    gotoClipActivity(Uri.fromFile(tempFile));
+                }
+                break;
+            case REQUEST_PICK:  //调用系统相册返回
+                if (resultCode == RESULT_OK) {
+                    Uri uri = intent.getData();
+                    gotoClipActivity(uri);
+                }
+                break;
+            case REQUEST_CROP_PHOTO:  //剪切图片返回
+                if (resultCode == RESULT_OK) {
+                    final Uri uri = intent.getData();
+                    if (uri == null) {
+                        return;
+                    }
+                    String cropImagePath = FileUtil.getRealFilePathFromUri(getApplicationContext(), uri);
+                    Bitmap bitMap = BitmapFactory.decodeFile(cropImagePath);
+                    imageView.setImageBitmap(bitMap);
+//                    if (type == 1) {
+//                        headImage1.setImageBitmap(bitMap);
+//                    } else {
+//                        headImage2.setImageBitmap(bitMap);
+//                    }
+                    //此处后面可以将bitMap转为二进制上传后台网络
+                    //......
+
+                }
+                break;
+        }
+    }
+    /**
+     * 打开截图界面
+     */
+    public void gotoClipActivity(Uri uri) {
+        if (uri == null) {
+            return;
+        }
+        Intent intent = new Intent();
+        intent.setClass(this, ClipImageActivity.class);
+//        intent.putExtra("type", type);
+        intent.setData(uri);
+        startActivityForResult(intent, REQUEST_CROP_PHOTO);
+    }
+
+    /**
+     * 外部存储权限申请返回
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == WRITE_EXTERNAL_STORAGE_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission Granted
+                gotoCamera();
+            }
+        } else if (requestCode == READ_EXTERNAL_STORAGE_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission Granted
+//                gotoPhoto();
+            }
+        }
+    }
+
+    /**
+     * 跳转到照相机
+     */
+    private void gotoCamera() {
+        Log.d("evan", "*****************打开相机********************");
+        try {
+            //创建拍照存储的图片文件
+            tempFile = new File(FileUtil.checkDirPath(Environment.getExternalStorageDirectory().getPath() + "/image/"), System.currentTimeMillis() + ".jpg");
+
+            //跳转到调用系统相机
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                //设置7.0中共享文件，分享路径定义在xml/file_paths.xml
+                intent.setFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                Uri contentUri = FileProvider.getUriForFile(MainActivity.this, BuildConfig.APPLICATION_ID + ".fileProvider", tempFile);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, contentUri);
+            } else {
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(tempFile));
+            }
+            startActivityForResult(intent, REQUEST_CAPTURE);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/huehn/initword/component/activity/clip/InnerView.java
@@ -0,0 +1,249 @@
+package com.huehn.initword.component.activity.clip;
+
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.graphics.RectF;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import com.huehn.initword.R;
+
+
+public class InnerView extends View {
+
+    private boolean canMove = false;
+    private View outSideView = null;
+    private float width;
+    private float height;
+    private float borderWidth;
+    private int borderColor;
+
+    private Paint paint;
+    private Path path;
+    private RectF rectF = new RectF();
+    private Point pointLeftTop = null;
+    private Point pointRightTop = null;
+    private Point pointLeftBottom = null;
+    private Point pointRightBottm = null;
+    private boolean hasInit = false;
+    private InnerViewBuilder innerViewBuilder;
+    public static final int DEFAULT_COLOR = R.color.color_f23324;
+    public static final int DEFAULT_BORDER = 10;
+
+    public InnerView(Context context, InnerViewBuilder innerViewBuilder) {
+        super(context);
+        this.innerViewBuilder = innerViewBuilder;
+        initView(null, innerViewBuilder);
+    }
+
+    public InnerView(Context context) {
+        super(context);
+    }
+
+    public InnerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        initView(attrs, null);
+    }
+
+    public InnerView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView(attrs, null);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if (outSideView != null && hasInit){
+            path.reset();
+            rectF.left = 0;
+            rectF.top = 0;
+            rectF.right = width;
+            rectF.bottom = height;
+            path.addRect(rectF, Path.Direction.CW);
+            canvas.drawPath(path, paint);
+        }
+
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+    @Override
+    public void layout(int l, int t, int r, int b) {
+        super.layout(l, t, r, b);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(measureWidth((int) width), measureHeight((int) height));
+
+    }
+
+    private int measureWidth(int measureSpec){
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+        //设置一个默认值，就是这个View的默认宽度为500，这个看我们自定义View的要求
+        int result = (int) width;
+        if (specMode == MeasureSpec.AT_MOST) {//相当于我们设置为wrap_content
+            result = specSize;
+        } else if (specMode == MeasureSpec.EXACTLY) {//相当于我们设置为match_parent或者为一个具体的值
+            result = specSize;
+        }
+        return result;
+    }
+
+    private int measureHeight(int measureSpec) {
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+        int result = (int) height;
+        if (specMode == MeasureSpec.AT_MOST) {
+            result = specSize;
+        } else if (specMode == MeasureSpec.EXACTLY) {
+            result = specSize;
+        }
+        return result;
+    }
+
+    /**
+     * java代码，构建者模式进来
+     * @param innerViewBuilder
+     */
+    private void initBuilderView(InnerViewBuilder innerViewBuilder){
+        if (innerViewBuilder != null){
+            canMove = innerViewBuilder.canMove;
+            outSideView = innerViewBuilder.outSideView;
+            width = innerViewBuilder.width;
+            height = innerViewBuilder.height;
+            borderWidth = innerViewBuilder.borderWidth;
+            borderColor = innerViewBuilder.borderColor;
+        }
+    }
+
+    /**
+     *
+     * @param attrs
+     */
+    private void initView(AttributeSet attrs, InnerViewBuilder innerViewBuilder){
+        //从xml进来
+        if (attrs != null && getContext() != null){
+            TypedArray array = getContext().obtainStyledAttributes(attrs, R.styleable.InnerView);
+            if (array == null){
+                return;
+            }
+            canMove = array.getBoolean(R.styleable.InnerView_can_move, false);
+            int outSideId = array.getResourceId(R.styleable.InnerView_out_side_view, 0);
+            if (outSideId > 0){
+                outSideView = (ViewGroup) View.inflate(getContext(), outSideId, null);
+            }
+            width = array.getDimension(R.styleable.InnerView_inner_width, 100);
+            height = array.getDimension(R.styleable.InnerView_inner_height, 100);
+            borderWidth = array.getDimension(R.styleable.InnerView_inner_border_width, 2);
+            borderColor = array.getColor(R.styleable.InnerView_inner_border_width, (DEFAULT_COLOR));
+            array.recycle();
+            initPaint();
+            initSize();
+        }else if (innerViewBuilder != null){
+            initBuilderView(innerViewBuilder);
+            initPaint();
+            initSize();
+        }
+
+    }
+
+    private void initPaint(){
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setColor(getResources().getColor(borderColor));
+        paint.setStrokeWidth(borderWidth);
+        paint.setStyle(Paint.Style.STROKE);
+        path = new Path();
+    }
+
+    private void initSize(){
+        if (outSideView != null){
+            int parentWidth = outSideView.getWidth();
+            int parentHeight = outSideView.getHeight();
+            pointLeftTop = new Point((int) ((parentWidth - width) / 2), 0);
+            pointRightTop = new Point((int) ((parentWidth + width) / 2), 0);
+            pointLeftBottom = new Point(0, (int) height);
+            pointRightBottm = new Point((int) ((parentWidth - width) / 2), (int) height);
+            hasInit = true;
+        }
+    }
+
+    public static class InnerViewBuilder{
+        private Context context;
+        private boolean canMove = false;
+        private View outSideView = null;
+        private float width;
+        private float height;
+        private float borderWidth;
+        private int borderColor;
+
+        public InnerViewBuilder(Context context) {
+            this.context = context;
+        }
+
+        public InnerViewBuilder setCanMove(boolean canMove) {
+            this.canMove = canMove;
+            return this;
+        }
+
+        public InnerViewBuilder setOutSideView(View outSideView) {
+            this.outSideView = outSideView;
+            return this;
+        }
+
+        public InnerViewBuilder setWidth(float width) {
+            this.width = width;
+            return this;
+        }
+
+        public InnerViewBuilder setHeight(float height) {
+            this.height = height;
+            return this;
+        }
+
+        public InnerViewBuilder setBorderWidth(float borderWidth) {
+            this.borderWidth = borderWidth;
+            return this;
+        }
+
+        public InnerViewBuilder setBorderColor(int borderColor) {
+            this.borderColor = borderColor;
+            return this;
+        }
+
+        public InnerView create(){
+            if (this.width == 0){
+                this.width = 100;
+            }
+            if (this.height == 0){
+                this.height = 100;
+            }
+            if (this.borderColor <= 0){
+                this.borderColor = DEFAULT_COLOR;
+            }
+            if (this.borderWidth <= 0){
+                this.borderWidth = DEFAULT_BORDER;
+            }
+            InnerView innerView = new InnerView(this.context, this);
+            ((RelativeLayout)outSideView.getParent()).addView(innerView);
+            return innerView;
+        }
+
+    }
+
+}
